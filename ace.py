39384a5cad87f9111a2b35990a645557147a7079
guan xing <cris_gx@163.com>
2026-06-06 20:20:27 +0800

    更改为acr

---
+++ ace.py
@@ -0,0 +1,10 @@
+import os 
+import numpy as np
+import random
+a=random.choice([1, 2, 3, 4, 5])
+print(a)
+print(np.zeros((3, 3)))
+os.makedirs('logs', exist_ok=True)
+
+
+os.makedirs('checkpoints', exist_ok=True)
